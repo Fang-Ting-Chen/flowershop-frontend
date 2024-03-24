2cf2fbb681aef1e09197f265b184159d94d4cf4c
Fang-Ting Chen <chenf891127@gmail.com>
2024-03-24 17:32:29 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,19 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import ViteImages from 'vite-plugin-vue-images';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/flowershop-frontend.github.io/',
   plugins: [
     vue(),
+    ViteImages({
+      dirs: ['src/assets/images'],
+      extensions: ['jpg', 'jpeg', 'png', 'svg', 'webp'],
+      
+    }),
+
   ],
   resolve: {
     alias: {

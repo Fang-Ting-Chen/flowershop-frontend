d11d26aec2fb2570c3fc3c482a125f52699da568
Fang-Ting-Chen <81439634+Fang-Ting-Chen@users.noreply.github.com>
2024-03-24 23:45:18 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': new URL('./src', import.meta.url).pathname,
     }
   }
 })
